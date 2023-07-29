d720beee0dabf42cdf718b607c310468a6e98b33
ibrahim hossain <ibrahimhossaincse@gmail.com>
2023-07-29 13:55:06 +0600

    setup successful the typeScript and tailwindcss to my personal  project

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var name:string[] = [];
+  var object: any = {
+    humanName : 'jewel',
+    home: "unknown",
+    age: 8800
+  };
+  for(const value in object){
+    name.push(object[value]);
+  };
   return (
-    <div className="App">
+    <div className="h-1">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="w-16 md:w-32 lg:w-48" alt="logo" />
       </header>
     </div>
   );
